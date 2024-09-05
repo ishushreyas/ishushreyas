e26b97b3645f54d0781a51ad292ed529c5ade3aa
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-06 01:55:46 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="p-3 footer flex j-s a-i">
         <p className="m-0 courier-prime">©2024 All Rights are reserved</p>
-        <a href="/forum"><span className="material-symbols-rounded c-1 p-1">forum</span></a>
+        <Link to="/forum"><span className="material-symbols-rounded c-1 p-1">forum</span></Link>
     </footer>
   );
 };
