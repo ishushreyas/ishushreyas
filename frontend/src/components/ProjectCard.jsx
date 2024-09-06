3addbca660f8220d21f67fe5e26200e31ee43b53
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-06 07:57:19 +0530

    Update ProjectCard.jsx

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -7,13 +7,11 @@ const ProjectCard = ({ project }) => {
       <Link className="td-0 c-0" to={project.link}>
         <h3 className="t-6 m-0 mt-2 righteous">{project.title}</h3> 
       </Link>
-      <br />
       <div className="flex j-s a-s">
       {project.tags.map((item) => {
         return <li className="p-2 m-4 bs-1 br-1 ls-0 c-1 courier-prime" key={item}>{item}</li>
       })}
       </div>
-      <br />
         <p className="t-5 poppins">{project.description}</p>
     </div>
   );
