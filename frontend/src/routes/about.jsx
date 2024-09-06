b1b22a76082236248b986fabe3a333b2fd60adf7
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-06 13:52:47 +0530

    Update about.jsx

--- frontend/src/routes/about.jsx
+++ frontend/src/routes/about.jsx
@@ -2,6 +2,27 @@ const AboutMe = () => {
   return (
     <div className="m-1 p-1">
       <h1 className="righteous">About Me</h1>
+      <h2 className="t-6 poppins">Software</h2>
+      <div className="m-3">
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">VS Code</span>
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">Android Studio</span>
+      </div>
+      <h2 className="t-6 poppins">Languages</h2>
+      <div className="m-3">
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">Go/GoLang</span>
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">JavaScript</span>
+      </div>
+      <h2 className="t-6 poppins">Interested In</h2>
+      <div className="m-3">
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">React</span>
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">C++</span>
+      </div>
+      <h2 className="t-6 poppins">Other Interests</h2>
+      <div className="m-3">
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">Japanese</span>
+        <span className="bs-2 br-4 bg-1 m-1 p-2 courier-prime">Sketching</span>
+      </div>
+      <h2 className="t-6 poppins">Work</h2>
       <p className="poppins">Currently I am working as a founding member of <a href="https://bnstech.online/">BnS Tech</a>.</p>
     </div>
   );
