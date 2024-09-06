993015637e69545dd0a60b55f51b9fad9ec275c2
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-06 08:30:53 +0530

    Update services.jsx

--- frontend/src/routes/services.jsx
+++ frontend/src/routes/services.jsx
@@ -6,32 +6,24 @@ const Services = () => {
       <h1 className="righteous">Services</h1>
       <p className="poppins">Here's the list of tools I use.</p>
       <h2 className="t-6 poppins">Software</h2>
-      <div className="p-1">
-        <ul>
-          <li className="bs-1 br-1">VS Code</li>
-          <li className="bs-1 br-1">Android Studio</li>
-        </ul>
+      <div className="m-3">
+        <span className="bs-1 br-1 bg-1 m-1 p-1">VS Code</span>
+        <span className="bs-1 br-1 bg-1 m-1 p-1">Android Studio</span>
       </div>
       <h2 className="t-6 poppins">Languages</h2>
-      <div className="p-1">
-        <ul>
-          <li className="bs-1 br-1">Go/Golang</li>
-          <li className="bs-1 br-1">JavaScript</li>
-        </ul>
+      <div className="m-3">
+        <span className="bs-1 br-1 bg-1 m-1 p-1">Go/GoLang</span>
+        <span className="bs-1 br-1 bg-1 m-1 p-1">JavaScript</span>
       </div>
       <h2 className="t-6 poppins">Interested In</h2>
-      <div className="p-1">
-        <ul>
-          <li className="bs-1 br-1">React</li>
-          <li className="bs-1 br-1">C++</li>
-        </ul>
+      <div className="m-3">
+        <span className="bs-1 br-1 bg-1 m-1 p-1">React</span>
+        <span className="bs-1 br-1 bg-1 m-1 p-1">C++</span>
       </div>
       <h2 className="t-6 poppins">Other Interests</h2>
-      <div className="p-1">
-        <ul>
-          <li className="bs-1 br-1">Learning Japanese</li>
-          <li className="bs-1 br-1">Sketching</li>
-        </ul>
+      <div className="m-3">
+        <span className="bs-1 br-1 bg-1 m-1 p-1">Learning Japanese</span>
+        <span className="bs-1 br-1 bg-1 m-1 p-1">Sketching</span>
       </div>
     </div>
   );
