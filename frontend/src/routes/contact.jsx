8433c0adcec91c82e924615193ff4460d6e648de
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-07 10:29:02 +0000

    Contact form update

--- frontend/src/routes/contact.jsx
+++ frontend/src/routes/contact.jsx
@@ -1,37 +1,43 @@
+import { useRef } from 'react';
 import { Form } from 'react-router-dom';
 
-function handleOnSubmit() {
-  const formData = {
-    name: "Ishu",
-    email: "some@email",
-    message: "long \nmulti line \nmessage"
-};
-
-  fetch('https://redesigned-tribble-5746wq649xpf47g7-8080.app.github.dev/contact', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-})
-.then(response => response.text())
-.then(data => alert(data))
-.catch(error => console.error('Error:', error));
-}
-
 export default function Contact() {
+  const SERVER_URL = "https://ishushreyas-production.up.railway.app/";
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const handleOnSubmit = () => {
+    const formData = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      message: messageRef.current.value
+  };
+
+  fetch( SERVER_URL , {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  })
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+  }
+
   return (
     <div className="m-1 p-1">
       <h1 className="righteous">Contact Me</h1>
       <label className="flex j-l a-i"><span className="material-symbols-rounded">person</span><p className="poppins">Name: </p></label>
-      <input type="text" placeholder="John Smith" className="bs-1 bd-1 br-2 t-5 p-2 poppins"/>
+      <input type="text" placeholder="John Smith" ref={nameRef} className="bs-1 bd-1 br-2 t-5 p-2 poppins"/>
       <label className="flex j-l a-i"><span className="material-symbols-rounded">email</span><p className="poppins">Email: </p></label>
-      <input type="email" placeholder="someone@gmail.com" className="bs-1 bd-1 br-2 t-5 p-2 poppins"/>
+      <input type="email" placeholder="someone@gmail.com" ref={emailRef} className="bs-1 bd-1 br-2 t-5 p-2 poppins"/>
       <label className="flex j-l a-i"><span className="material-symbols-rounded">message</span><p className="poppins">Message: </p></label>
-      <textarea placeholder="Message..." className="bs-1 bd-1 br-2 t-5 p-2 poppins"></textarea> <br />
+      <textarea placeholder="Message..." ref={messageRef} className="bs-1 bd-1 br-2 t-5 p-2 poppins"></textarea> <br />
       <Form
             method="post"
-            action="destroy"
+            action="contact"
             onSubmit={(event) => {
               event.preventDefault();
               handleOnSubmit();
