b83b5ca5f177bc07249661ad5e8e2e47f52b41b0
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-07 13:45:44 +0000

    service

--- frontend/src/routes/services.jsx
+++ frontend/src/routes/services.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className="m-1 p-1">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <h1 className="righteous">Services</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className='bs-2 br-2 bg-1 m-1 p-1'>
         <h3 className='t-6 poppins'>Static Website</h3>
         <p className='poppins'>I use React framework to create Static website.</p>
       </div>
       <div className='bs-2 br-2 bg-1 m-1 p-1'>
-        <h3 className='t-6 poopins'>Full Stack</h3>
+        <h3 className='t-6 poppins'>Full Stack</h3>
         <p className='poppins'>I use React Js for frontend and GoLang for backend. This site itself is a full stack website created using React Js and GoLang.</p>
       </div>
       <div className='bs-2 br-2 bg-1 m-1 p-1'>
