a4af476251cb126db014a7f6a9a077d0191a97e3
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-11 05:23:03 +0000

    bg

--- frontend/src/routes/about.jsx
+++ frontend/src/routes/about.jsx
@@ -7,23 +7,23 @@ const AboutMe = () => {
       <p className="poppins">I am a full stack developer uses <i>React</i> and <i>GoLang</i> for <b>Web Development</b> and <i>Java</i> for <b>Android Development</b>. Recently, I am exploring C++.</p>
       <h2 className="t-6 poppins">Software</h2>
       <div className="m-3">
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">VS Code</span>
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">Android Studio</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">VS Code</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">Android Studio</span>
       </div>
       <h2 className="t-6 poppins">Languages</h2>
       <div className="m-3">
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">Go/GoLang</span>
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">JavaScript</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">Go/GoLang</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">JavaScript</span>
       </div>
       <h2 className="t-6 poppins">Interested In</h2>
       <div className="m-3">
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">React</span>
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">C++</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">React</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">C++</span>
       </div>
       <h2 className="t-6 poppins">Other Interests</h2>
       <div className="m-3">
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">Japanese</span>
-        <span className="bs-3 br-4 bg-1 m-1 p-2 courier-prime">Sketching</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">Japanese</span>
+        <span className="bs-1 br-4 bg-2 m-1 p-2 courier-prime">Sketching</span>
       </div>
       <h2 className="t-6 poppins">Work</h2>
       <p className="poppins">Currently I am working as a founding member of <Link className="td-0 c-1 courier-prime" to="https://bnstech.online/">BnS Tech</Link>.</p>
