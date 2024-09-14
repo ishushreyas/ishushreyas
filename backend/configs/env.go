3ceeccb8af0afd4a6ea7121688028c44b187fc40
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-14 18:54:57 +0000

    env

--- backend/configs/env.go
+++ backend/configs/env.go
@@ -10,7 +10,7 @@ import (
 func EnvMongoURI() string {
     err := godotenv.Load()
     if err != nil {
-        log.Fatal("Error loading .env file")
+        log.Panicln("Error loading .env file")
     }
 
     return os.Getenv("MONGOURI")
