369ba76611cac81bcc5483e1de85f6a13d9b922a
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-17 22:29:44 +0530

    Update contact.jsx

--- frontend/src/routes/contact.jsx
+++ frontend/src/routes/contact.jsx
@@ -35,11 +35,11 @@ export default function Contact() {
       <div className="flex j-c a-i">
         <div className='bs-1 bd-1 bg-2 br-2 p-3 cw-1'>
           <label className="flex j-l a-i"><span className="material-symbols-rounded">person</span><p className="poppins">Name: </p></label>
-          <input type="text" name='name' placeholder="John Smith" onChange={onChangeHandler} className="bs-2 bg-3 bd-1 br-2 cw-2 t-5 p-2 poppins"/>
+          <input type="text" name='name' placeholder="John Smith" onChange={onChangeHandler} className="bs-2 br-2 cw-2 t-5 p-2 poppins"/>
           <label className="flex j-l a-i"><span className="material-symbols-rounded">email</span><p className="poppins">Email: </p></label>
-          <input type="email" name='email' placeholder="someone@gmail.com" onChange={onChangeHandler} className="bs-2 bg-3 bd-1 br-2 cw-2 t-5 p-2 poppins"/>
+          <input type="email" name='email' placeholder="someone@gmail.com" onChange={onChangeHandler} className="bs-2 br-2 cw-2 t-5 p-2 poppins"/>
           <label className="flex j-l a-i"><span className="material-symbols-rounded">message</span><p className="poppins">Message: </p></label>
-          <textarea name='message' placeholder="Message..." onChange={onChangeHandler} className="bs-2 bg-3 bd-1 br-2 cw-2 t-5 p-2 poppins"></textarea> <br />
+          <textarea name='message' placeholder="Message..." onChange={onChangeHandler} className="bs-2 br-2 cw-2 t-5 p-2 poppins"></textarea> <br />
           <Form
                 method="post"
                 action="contact"
@@ -48,7 +48,7 @@ export default function Contact() {
                   handleOnSubmit();
                 }}
           >
-              <button className="bs-2 bd-1 bg-0 t-5 m-3 p-3 br-1 poppins">Send</button>
+              <button className="bs-2 t-5 m-3 p-3 br-1 poppins">Send</button>
           </Form>
         </div>
       </div>
