3221854ed9ed5efabe2d6d8276f74df2eeab63f7
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-20 14:36:54 +0530

    Update contents.jsx

--- frontend/src/routes/contents.jsx
+++ frontend/src/routes/contents.jsx
@@ -26,17 +26,16 @@ export function Contents() {
         <ul className='m-0 p-0'>
           {contents.map((content) => (
             <li className='ls-0' key={content.ID}>
-              <div className='m-1 p-1 bg-1 br-1 hv:cd-1 poppins'>
               <Link className='td-0 c-0' to={`/contents/${content.ID}`}>
+              <div className='p-1 hv:cd-1 p'>
                 <p>{content.Title || <i>No Title</i>}</p>
                 {content.Tags && content.Tags.length > 0 && (
                   <span>{content.Tags.map((tag) => (
                     <span className='p-2 m-1 bs-3 bg-3 br-1 ls-0 c-1 courier-prime'>{ tag }</span>
                   ))}</span>
                 )}
-                <p>{content.Author && <span> - {content.Author}</span>}</p>
+                </div>
               </Link>
-              </div>
             </li>
           ))}
         </ul>
