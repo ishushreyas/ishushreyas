961ac87bc6015eaaa1391af8804e588f48d36328
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-20 15:07:17 +0530

    Update ProjectCard.jsx

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -6,9 +6,9 @@ const ProjectCard = ({ project }) => {
     <div className="br-2 m-1 p-1 bg-2 hv:cd-1">
       <Link className="td-0 c-0" to={project.link}>
         <h3 className="t-6 m-0 mt-2 p">{project.title}</h3> 
-        <div className="flex wrap p-1">
+        <div className="flex wrap">
           {project.tags.map((item) => {
-              return <span className="p-2 m-1 bd-1 bg-3 br-1 c-1 courier-prime" key={item}>{item}</span>
+              return <span className="p-2 m-0 bd-1 bg-3 br-1 c-1 courier-prime" key={item}>{item}</span>
           })}
         </div>
           <p className="m-0 t-5 p">{project.description}</p>
