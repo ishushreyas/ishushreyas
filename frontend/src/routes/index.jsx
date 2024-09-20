e7c1050a73914d5cfc01f40087518ccd9a659c27
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-20 12:13:13 +0530

    Update index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,6 +4,7 @@ export default function Index() {
       <h3 className="t-2 m-0 p-0 poppins">Hi, My Name is</h3>
       <h1 className="t-1 m-0 p-0 text-grad text-anim righteous">Ishu Shreyas</h1>
       <p className="t-3 m-0 p-0 poppins">I am a curious Developer.</p>
+      <iframe src="https://github.com/sponsors/ishushreyas/button" title="Sponsor ishushreyas" height="32" width="114" style="border: 0; border-radius: 6px;"></iframe>
     </div>
   );
 }
