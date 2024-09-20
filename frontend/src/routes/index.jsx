bcb3eded7d54dcfc80f844d4495fef832fcf7a20
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-20 14:41:24 +0530

    Update index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,9 +1,9 @@
 export default function Index() {
   return (
     <div className="m-1 p-1">
-      <h3 className="t-2 m-0 p-0 poppins">Hi, My Name is</h3>
+      <h3 className="t-2 m-0 p-0 p">Hi, My Name is</h3>
       <h1 className="t-1 m-0 p-0 text-grad text-anim righteous">Ishu Shreyas</h1>
-      <p className="t-3 m-0 p-0 poppins">I am a curious Developer.</p>
+      <p className="t-3 m-0 p-0 p">I am a curious Developer.</p>
       <iframe src="https://github.com/sponsors/ishushreyas/button" title="Sponsor ishushreyas" height="32" width="114" style={{border: '0', borderRadius: '6px'}}></iframe>
     </div>
   );
