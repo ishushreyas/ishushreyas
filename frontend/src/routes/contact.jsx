e770f533b07af71c10ed5e9b4f4fc83054aee74d
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-20 14:19:43 +0530

    Update contact.jsx

--- frontend/src/routes/contact.jsx
+++ frontend/src/routes/contact.jsx
@@ -34,12 +34,12 @@ export default function Contact() {
       <h1 className="righteous">Contact Me</h1>
       <div className="flex j-c a-i">
         <div className='bs-2 bd-1 bg-1 br-2 p-3 cw-1'>
-          <label className="flex j-l a-i"><span className="material-symbols-rounded">person</span><p className="poppins">Name: </p></label>
-          <input type="text" name='name' placeholder="John Smith" onChange={onChangeHandler} className="cw-2 poppins"/>
-          <label className="flex j-l a-i"><span className="material-symbols-rounded">email</span><p className="poppins">Email: </p></label>
-          <input type="email" name='email' placeholder="someone@gmail.com" onChange={onChangeHandler} className="cw-2 poppins"/>
-          <label className="flex j-l a-i"><span className="material-symbols-rounded">message</span><p className="poppins">Message: </p></label>
-          <textarea name='message' placeholder="Message..." onChange={onChangeHandler} className="cw-2 poppins"></textarea> <br />
+          <label className="flex j-l a-i"><span className="material-symbols-rounded">person</span><p className="p">Name: </p></label>
+          <input type="text" name='name' placeholder="John Smith" onChange={onChangeHandler} className="bd-1 cw-2 p"/>
+          <label className="flex j-l a-i"><span className="material-symbols-rounded">email</span><p className="p">Email: </p></label>
+          <input type="email" name='email' placeholder="someone@gmail.com" onChange={onChangeHandler} className="bd-1 cw-2 p"/>
+          <label className="flex j-l a-i"><span className="material-symbols-rounded">message</span><p className="p">Message: </p></label>
+          <textarea name='message' placeholder="Message..." onChange={onChangeHandler} className="bd-1 cw-2 p"></textarea> <br />
           <Form
                 method="post"
                 action="contact"
@@ -48,7 +48,7 @@ export default function Contact() {
                   handleOnSubmit();
                 }}
           >
-              <button className="bs-2 t-5 m-3 p-3 br-1 poppins">Send</button>
+              <button className="bd-1 m-3 p-3 br-1 p">Send</button>
           </Form>
         </div>
       </div>
