cd7ee1d8ca105c2213e7c3c7c992bc4970c06be5
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-09-30 10:13:48 +0530

    Update contents.jsx

--- frontend/src/routes/contents.jsx
+++ frontend/src/routes/contents.jsx
@@ -31,7 +31,7 @@ export function Contents() {
                 <p>{content.Title || <i>No Title</i>}</p>
                 {content.Tags && content.Tags.length > 0 && (
                   <span>{content.Tags.map((tag) => (
-                    <span className='p-2 m-1 bs-3 bg-3 br-1 ls-0 c-1 courier-prime'>{ tag }</span>
+                    <span key={index} className='p-2 mr-1 bs-3 glass bd-1 br-1 ls-0 c-2 fw-1'>{tag}</span>
                   ))}</span>
                 )}
                 </div>
