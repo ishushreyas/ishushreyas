37f499fe91b9e6179b78f6ca6536db8a03d0ce5f
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-10-26 23:39:25 +0530

    Add files via upload

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({ project }) => {
         <h3 className="t-6 m-0 mt-2 p">{project.title}</h3> 
         <div className="flex wrap">
           {project.tags.map((item) => {
-              return <span className="p-2 m-0 bd-1 bg-3 br-1 c-1 courier-prime" key={item}>{item}</span>
+              return <span className="p-2 m-0 br-2 c-1 courier-prime" key={item}>{item}</span>
           })}
         </div>
           <p className="m-0 t-5 p">{project.description}</p>
