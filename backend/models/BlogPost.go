fe742bd079b2a47e69a5e04d0bf3c0036d14a4f5
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-11-02 12:25:22 +0530

    Update BlogPost.go

--- backend/models/BlogPost.go
+++ backend/models/BlogPost.go
@@ -5,16 +5,22 @@ import (
     "time"
 )
 
+// ContentItem represents a single piece of content, either markdown or image
+type ContentItem struct {
+    Type string `bson:"type"` // "markdown" or "image"
+    Data string `bson:"data"` // Markdown text or image URL
+}
+
 // BlogPost represents the structure of a blog post
 type BlogPost struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"` // MongoDB ID field
-	Link      string             `baon:"link"`
-	Title     string             `bson:"title"`          // Blog post title
-	Image     string             `bson:"image"`          // Link for blog image
-	Color     string             `bson:"color"`
-	Content   string             `bson:"content"`        // Main content of the blog post
-	Author    string             `bson:"author"`         // Author's name
-	Tags      []string           `bson:"tags,omitempty"` // Tags for categorization (optional)
-	CreatedAt time.Time          `bson:"createdAt"`      // Timestamp of creation
-	UpdatedAt time.Time          `bson:"updatedAt"`      // Timestamp of last update
+    ID          primitive.ObjectID `bson:"_id,omitempty"` // MongoDB ID field
+    Link        string             `bson:"link"`
+    Title       string             `bson:"title"`          // Blog post title
+    Image       string             `bson:"image"`          // Link for blog image
+    Color       string             `bson:"color"`
+    ContentItems []ContentItem      `bson:"contentItems"`   // Array of markdown and image content
+    Author      string             `bson:"author"`         // Author's name
+    Tags        []string           `bson:"tags,omitempty"` // Tags for categorization (optional)
+    CreatedAt   time.Time          `bson:"createdAt"`      // Timestamp of creation
+    UpdatedAt   time.Time          `bson:"updatedAt"`      // Timestamp of last update
 }
